9d3c56afd3163700b28c44289dbebadf90983f21
alexandrekm <alexandre.keunecke@gmail.com>
2020-01-28 13:36:29 -0800

    Update webview.js
    improving the message counter

--- webview.js
+++ webview.js
@@ -1,7 +1,28 @@
 module.exports = Franz => {
     function getMessages() {
-        const count = document.querySelector('.navigationItem-counter').innerText
-        Franz.setBadge(count ? count.substring(1, count.length - 1) : 0)
+        // input the room names you want the unread messages to be counted in the badge
+		var roomsWithNotifications = ["YOUR_ROOM_NAMES"];
+        const roomList = document.querySelectorAll(".RoomListItemContainer");
+
+        var totalMessages = 0;
+		Array.prototype.forEach.call(roomList, function(room) {
+			var title = room.querySelector(".RoomListItemContainer__title").innerText.split('\n')[0];
+			var unread = room.querySelector(".RoomListItemContainer__unreadBadge")
+			if (unread) {
+				console.log(title);
+				if (roomsWithNotifications.includes(title)){
+					console.log("room: "+ title + " has " + unread.innerText);
+					totalMessages += parseInt(unread.innerText);
+				}
+			}
+		});
+		const unreadPersonalMessages = document.querySelectorAll(".ConversationListItemContainer__unreadBadge");
+		Array.prototype.forEach.call(unreadPersonalMessages, function(unreadMessages) {
+			var unreadMessage = parseInt(unreadMessages.innerText.split('\n')[0]);
+			totalMessages += unreadMessage;
+		});
+		Franz.setBadge(totalMessages)		
     }
     Franz.loop(getMessages)
 }
+
